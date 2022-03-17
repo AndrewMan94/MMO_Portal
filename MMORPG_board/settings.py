42c28ed487f4365afcf2dd97e38008fea79e0aeb
Andrew94 <black-dazor@mail.ru>
2022-03-17 23:27:35 +0300

    123
    D.16

--- MMORPG_board/settings.py
+++ MMORPG_board/settings.py
@@ -217,8 +217,8 @@ DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 EMAIL_HOST = 'smtp.yandex.ru'
 EMAIL_PORT = 465
-EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 CELERY_BROKER_URL = 'redis://localhost:6379'
